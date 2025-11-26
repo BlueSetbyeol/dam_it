22c677aa4ed9805e05fc3c528f509eb0efc371be
BlueSetbyeol <setbyeol.c@gmail.com>
2025-11-26 16:05:07 +0100

    instead of DnD, buttons

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -34,18 +34,20 @@ export default function Quiz() {
   useEffect(() => {
     const random = Math.floor(Math.random() * questionsAvailable.length);
     setRandomQuestion(random);
-    console.log(randomQuestion);
   }, []);
-  console.log(randomQuestion);
 
   let firstQuestion;
   let secondQuestion;
+  const answersOfQuestions: string[][] = [];
 
   if (randomQuestion) {
     firstQuestion = objects_to_sort.find(
       (sort) =>
         sort.id === Number(questionsAvailable[randomQuestion].object_to_sort_id)
     );
+    if (firstQuestion) {
+      answersOfQuestions.push(firstQuestion.good_answer);
+    }
   }
   if (randomQuestion) {
     secondQuestion = objects_to_repair.find(
@@ -53,16 +55,97 @@ export default function Quiz() {
         sort.id ===
         Number(questionsAvailable[randomQuestion].object_to_repair_id)
     );
+    if (secondQuestion) {
+      answersOfQuestions.push(secondQuestion.good_answer);
+    }
   }
 
+  console.log(answersOfQuestions);
+
+  const [popUpClose, setPopUpClose] = useState(false);
+  const [beginToFinish, setBeginToFinish] = useState(true);
+  const [endGame, setEndGame] = useState(false);
+
+  const answerToKeep: string[] = [];
+  const [answerToCheck, setAnswerToCheck] = useState<string[]>([]);
+  console.log(answerToCheck);
+
+  let score = 0;
+
+  const checkingAnswer = (
+    answersOfQuestions: string[][],
+    answerToCheck: string[]
+  ) => {
+    if (answersOfQuestions && answerToCheck) {
+      answersOfQuestions.forEach((answer) => {
+        answer.forEach((possibilities) => {
+          if (answerToCheck.includes(possibilities)) {
+            score += 1;
+          }
+        });
+      });
+    }
+  };
+
+  checkingAnswer(answersOfQuestions, answerToCheck);
+
   return (
-    <section>
-      <p>{firstQuestion?.mission}</p>
-      {firstQuestion?.answers.map((object) => (
-        <button type="button" key={object.name}>
-          {object.value}
-        </button>
-      ))}
-    </section>
+    <>
+      <section>
+        {beginToFinish ? (
+          !popUpClose ? (
+            <article>
+              <button type="button" onClick={() => setPopUpClose(true)}>
+                X
+              </button>
+              <p>{firstQuestion?.mission}</p>
+            </article>
+          ) : (
+            <article className="answer_zone">
+              {firstQuestion?.answers.map((object) => (
+                <button
+                  type="button"
+                  key={object.name}
+                  className="solutions"
+                  onClick={() => {
+                    answerToKeep.push(object.name);
+                    setAnswerToCheck(answerToKeep);
+                    setBeginToFinish(false);
+                    setPopUpClose(false);
+                  }}
+                >
+                  {object.value}
+                </button>
+              ))}
+            </article>
+          )
+        ) : !popUpClose ? (
+          <article>
+            <button type="button" onClick={() => setPopUpClose(true)}>
+              X
+            </button>
+            <p>{secondQuestion?.mission}</p>
+          </article>
+        ) : (
+          <article className="answer_zone">
+            {secondQuestion?.answers.map((object) => (
+              <button
+                type="button"
+                key={object.name}
+                className="solutions"
+                onClick={() => {
+                  answerToKeep.push(object.name);
+                  setAnswerToCheck(answerToKeep);
+                  setEndGame(false);
+                }}
+              >
+                {object.value}
+              </button>
+            ))}
+          </article>
+        )}
+      </section>
+      {endGame && <p>Your result is : {score}/2</p>}
+    </>
   );
 }
